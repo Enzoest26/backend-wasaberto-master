ff970f7661685c403a921663968e672719a84f4c
Enzoest26 <enzoest26@gmail.com>
2025-01-18 16:31:52 -0500

    initial project

---
+++ src/main/java/com/wasaberto/master/BackendWasabertoMasterApplication.java
@@ -0,0 +1,13 @@
+package com.wasaberto.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWasabertoMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWasabertoMasterApplication.class, args);
+    }
+
+}
